3f3e193d8f14fe0d017a601bf9677f06dfeb843d
cadan <cadan85@gmail.com>
2014-11-22 23:10:31 +0200

    Commit BroadCastRecieverExample

---
+++ BroadCastRecieverExample/src/il/ac/shenkar/broadcastrecieverexample/common/AppConst.java
@@ -0,0 +1,6 @@
+package il.ac.shenkar.broadcastrecieverexample.common;
+
+public class AppConst {
+	public static final String Extra_Message = "Message";
+	public static final String ACTION_ALARM = "il.ac.shenkar.ALARM";
+}
